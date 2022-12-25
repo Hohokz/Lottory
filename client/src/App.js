6892f2dd20d25c39a58678c812c1fcabea129c08
Apiwat.S <hohokz@hotmail.com>
2022-12-25 14:28:38 +0700

    delete menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Menu from "./conponent/header.js";
 import RandomNumber from './conponent/random_number';
 import NumberForm from "./conponent/check.js";
 import './App.css'
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App-header">
       <>
-        <Menu />
         <RandomNumber />
         <NumberForm />
       </>
